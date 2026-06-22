56ff5b10807f94608827cb5d4836216a99b721d9
Yasemen KOÇ <98353304+Yasemenkilic@users.noreply.github.com>
2026-06-22 17:27:01 +0300

    Create sql-9.odev.sql

---
+++ sql-9.odev.sql
@@ -0,0 +1,9 @@
+--9.ödev
+select city ,country  from city 
+inner join country on city.city_id = country.country_id ;
+
+select payment_id, first_name, last_name from payment 
+inner join customer on payment.customer_id   = customer.customer_id ;
+
+select rental_id, first_name, last_name from rental 
+inner join customer on rental.customer_id = customer.customer_id ;
